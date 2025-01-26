129fe3bb45fb8fa288824b25c06ba96255ee6f76
Mikhail Skrobat <obgora228@gmail.com>
2025-01-26 23:34:02 +0100

    Add bottom Navigation

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,15 +1,15 @@
-import { ScrollView, Text, View, Image } from "react-native";
-import { icons, images } from "@/constants";
-import InputField from "@/components/InputField";
-import { useCallback, useState } from "react";
-import CustomButton from "@/components/CustomButton";
-import { Link, useRouter } from "expo-router";
-import OAuth from "@/components/OAuth";
 import { useSignIn } from "@clerk/clerk-expo";
+import { Link, router } from "expo-router";
+import { useCallback, useState } from "react";
+import { Alert, Image, ScrollView, Text, View } from "react-native";
+
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
+import { icons, images } from "@/constants";
 
 const SignIn = () => {
   const { signIn, setActive, isLoaded } = useSignIn();
-  const router = useRouter();
 
   const [form, setForm] = useState({
     email: "",
@@ -19,29 +19,25 @@ const SignIn = () => {
   const onSignInPress = useCallback(async () => {
     if (!isLoaded) return;
 
-    // Start the sign-in process using the email and password provided
     try {
       const signInAttempt = await signIn.create({
         identifier: form.email,
         password: form.password,
       });
 
-      // If sign-in process is complete, set the created session as active
-      // and redirect the user
       if (signInAttempt.status === "complete") {
         await setActive({ session: signInAttempt.createdSessionId });
-        router.replace("/");
+        router.replace("/(root)/(tabs)/home");
       } else {
-        // If the status isn't complete, check why. User might need to
-        // complete further steps.
-        console.error(JSON.stringify(signInAttempt, null, 2));
+        // See https://clerk.com/docs/custom-flows/error-handling for more info on error handling
+        console.log(JSON.stringify(signInAttempt, null, 2));
+        Alert.alert("Error", "Log in failed. Please try again.");
       }
-    } catch (err) {
-      // See https://clerk.com/docs/custom-flows/error-handling
-      // for more info on error handling
-      console.error(JSON.stringify(err, null, 2));
+    } catch (err: any) {
+      console.log(JSON.stringify(err, null, 2));
+      Alert.alert("Error", err.errors[0].longMessage);
     }
-  }, [isLoaded, form.email, form.password]);
+  }, [isLoaded, form]);
 
   return (
     <ScrollView className="flex-1 bg-white">
@@ -52,20 +48,24 @@ const SignIn = () => {
             Welcome 👋
           </Text>
         </View>
-        <View className="p-5 ">
+
+        <View className="p-5">
           <InputField
             label="Email"
-            icon={icons.email}
-            value={form.email}
             placeholder="Enter email"
+            icon={icons.email}
+            textContentType="emailAddress"
+            value={form.email}
             onChangeText={(value) => setForm({ ...form, email: value })}
           />
+
           <InputField
             label="Password"
+            placeholder="Enter password"
             icon={icons.lock}
             secureTextEntry={true}
+            textContentType="password"
             value={form.password}
-            placeholder="Enter password"
             onChangeText={(value) => setForm({ ...form, password: value })}
           />
 
@@ -81,7 +81,7 @@ const SignIn = () => {
             href="/sign-up"
             className="text-lg text-center text-general-200 mt-10"
           >
-            <Text>Don't have an account? </Text>
+            Don't have an account?{" "}
             <Text className="text-primary-500">Sign Up</Text>
           </Link>
         </View>
