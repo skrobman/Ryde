4a8b0e439c142e702acfffd86ecdaff7da51af4b
Mikhail Skrobat <obgora228@gmail.com>
2025-01-28 23:36:00 +0100

    Add some changes in location

---
+++ app/store/index.ts
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+import { LocationStore } from "@/types/type";
+
+export const useLocationStore = create<LocationStore>((set) => ({
+  userAddress: null,
+  userLongitude: null,
+  userLatitude: null,
+  destinationLongitude: null,
+  destinationLatitude: null,
+  destinationAddress: null,
+  setUserLocation: ({
+    latitude,
+    longitude,
+    address,
+  }: {
+    latitude: number;
+    longitude: number;
+    address: string;
+  }) => {
+    set(() => ({
+      userLongitude: longitude,
+      userLatitude: latitude,
+      userAddress: address,
+    }));
+  },
+  setDestinationLocation: ({
+    latitude,
+    longitude,
+    address,
+  }: {
+    latitude: number;
+    longitude: number;
+    address: string;
+  }) => {
+    set(() => ({
+      destinationLongitude: longitude,
+      destinationLatitude: latitude,
+      destinationAddress: address,
+    }));
+  },
+}));
