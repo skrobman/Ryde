d04aedcc622c2709a2316595d4e53335b4cd5f8a
Mikhail Skrobat <obgora228@gmail.com>
2025-01-28 18:16:44 +0100

    Add search ui component(wtht functionality)

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,8 +1,17 @@
 import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
 import { Link } from "expo-router";
-import { FlatList, Text } from "react-native";
+import {
+  FlatList,
+  Text,
+  View,
+  Image,
+  ActivityIndicator,
+  TouchableOpacity,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import RideCard from "@/components/RideCard";
+import { icons, images } from "@/constants";
+import GoogleTextInput from "@/components/GoogleTextInput";
 
 const recentRides = [
   {
@@ -113,12 +122,61 @@ const recentRides = [
 
 export default function Page() {
   const { user } = useUser();
+  const loading = true;
+
+  const handleSignOut = () => {};
+
+  const handleDestinationPress = () => {};
 
   return (
     <SafeAreaView className="bg-general-500">
       <FlatList
         data={recentRides?.slice(0, 5)}
         renderItem={({ item }) => <RideCard ride={item} />}
+        className="px-5"
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+        ListEmptyComponent={() => (
+          <View className="flex flex-col items-center justify-center">
+            {!loading ? (
+              <>
+                <Image
+                  source={images.noResult}
+                  className="w-40 h-40"
+                  alt="No recent rides found"
+                  resizeMode="contain"
+                />
+                <Text className="text-sm ">No recent rides found</Text>
+              </>
+            ) : (
+              <ActivityIndicator size="small" color="#000" />
+            )}
+          </View>
+        )}
+        ListHeaderComponent={() => (
+          <>
+            <View className="flex flex-row items-center justify-between my-5">
+              <Text className="text-2xl font-JakartaExtraBold capitalize">
+                Welcome{", "}
+                {user?.firstName ||
+                  user?.emailAddresses[0].emailAddress.split("@")[0]}
+              </Text>
+              <TouchableOpacity
+                onPress={handleSignOut}
+                className="justify-center items-center w-12 h-12 rounded-full bg-white"
+              >
+                <Image source={icons.out} className="w-6 h-6" />
+              </TouchableOpacity>
+            </View>
+            <GoogleTextInput
+              icon={icons.search}
+              containerStyle={"bg-white shadow-md shadow-neutral-300"}
+              handlePress={handleDestinationPress}
+            />
+          </>
+        )}
       />
     </SafeAreaView>
   );
